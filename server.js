077e6fec64208442b9cbdf2c524de77431a1db66
Aynur Cemre Aka <cemreaka@gmail.com>
2022-06-06 17:09:29 +0300

    port info is changed

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-
-app.listen(5000);
